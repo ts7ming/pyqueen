1ece2fc9609a685383fd38bc7d833e4ad0e2eb39
QimingMa <qiming.sci@outlook.com>
2023-11-09 14:11:51 +0800

    docs: 更新文档

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqueen',
-    version='1.0.7',
+    version='1.0.8',
     url='https://github.com/ts7ming/pyqueen.git',
     description='Rule your Data',
-    long_description=open("README.md",encoding='utf-8').read(),
+    long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     author='ts7ming',
     author_email='qiming.ma@outlook.com',
@@ -27,4 +27,9 @@ setup(
         'xlrd==1.2.0',
         'XlsxWriter'
     ],
+    entry_points={
+        'console_scripts': [
+            'pyqueen = pyqueen:cmd',
+        ]
+    }
 )
